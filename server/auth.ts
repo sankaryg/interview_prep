cfa0ceb6ed887d8c73f567ebc7a3d6a0c7e63841
ygs <ygsankar.87@gmail.com>
2025-02-20 15:45:21 +0530

    added dotenv package

--- server/auth.ts
+++ server/auth.ts
@@ -147,6 +147,17 @@ export function setupAuth(app: Express) {
     });
   });
 
+  app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+  }));
+
+  app.get("/auth/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/auth",
+      successRedirect: "/"
+    })
+  );
+
   app.get("/api/user", (req, res) => {
     if (!req.isAuthenticated()) return res.sendStatus(401);
     res.json(req.user);
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -50,6 +53,7 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
+  
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
